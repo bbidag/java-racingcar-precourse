0713b46479d73bfc20b082b6f9aa06fcc7e85e43
bbidag <skawlgh112@gmail.com>
2021-05-02 19:08:35 +0900

    fix: 차 이름이 빈 문자열인 경우 exception에 포함한다. trim() 도 적용함.

--- src/main/java/com/bbidag/racingcar/CarName.java
+++ src/main/java/com/bbidag/racingcar/CarName.java
@@ -5,13 +5,13 @@ public class CarName {
     private final String name;
 
     public CarName(String name){
-        if(name == null) {
-            throw new NullPointerException("차 이름은 null이면 안됩니다.");
+        if(name == null || name.trim().length() == 0) {
+            throw new NullPointerException("차 이름은 비어 있으면 안됩니다.");
         }
         if(name.length() > 5) {
             throw new IllegalArgumentException("차 이름은 5자를 초과하면 안됩니다.");
         }
-        this.name = name;
+        this.name = name.trim();
     }
 
     public String getName() {
